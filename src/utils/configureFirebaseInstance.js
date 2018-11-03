5d9e5ae5214b8dda6250197487ec7079c311d4cd
Mason Hall <masonhall@gmail.com>
2018-11-03 17:17:03 -0500

    sharing updates

--- src/utils/configureFirebaseInstance.js
+++ src/utils/configureFirebaseInstance.js
@@ -3,18 +3,33 @@ import * as firebase from 'firebase'
 // Configure your firebase singleton instance
 console.log("hello");
 
-const firebaseInstance = firebase.initializeApp({
+firebase.initializeApp({
   apiKey: "AIzaSyARisDaUpCv3QpAlve0arKpU69d1a0SSy4",
   authDomain: "bias-checker.firebaseapp.com",
   databaseURL: "https://bias-checker.firebaseio.com",
   storageBucket: "bias-checker.appspot.com",
 })
 
+export var database = firebase.database();
+
 /**
  * @desc Upserts some data to a Firebase real-time databaase
  */
+
 export const writeDataToFirebase = (articleId, data) => 
   firebase
     .database()
     .ref(`/articles/${articleId}`)
     .update(data)
+
+  export const readDataToFirebase = (articleId, data) => 
+  firebase
+    .database()
+    .ref(`/articles/${articleId}`)
+    .update(data)
+  
+  // var biasScoreRef = firebase.database().ref('articles/' + articleId + '/starCount');
+  // biasScoreRef.on('value', function(snapshot) {
+  //   updateBiasCount(postElement, snapshot.val());
+  // })
+  ;
